3fbf57e8d14637a76b83929d8947a0877975cf60
yosefstar <129289620+yosefstar@users.noreply.github.com>
2023-04-17 22:36:24 +0900

    add submodules

--- app.py
+++ app.py
@@ -23,6 +23,13 @@ import secrets
 import requests
 import random
 import asyncio
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver.get("https://codelabsjp.net")
 
 
 app = Flask(__name__, static_folder='static')
