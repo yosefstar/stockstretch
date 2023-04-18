949c3565b131bb415763fc390b4a710a8a493230
yosefstar <129289620+yosefstar@users.noreply.github.com>
2023-04-18 20:43:13 +0900

    Fix issues in app.py

--- app.py
+++ app.py
@@ -36,14 +36,11 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = set(['csv'])
 
 app.config['JSON_AS_ASCII'] = False
-app.secret_key = secrets.token_hex(16)
 
 # WebDriverを初期化する
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
-
-app = Flask(__name__)
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")  # 追加: 環境変数からChromeのバイナリの場所を取得
@@ -119,79 +116,6 @@ def csv_download():
     response.headers['Content-Disposition'] = 'attachment; filename=result.csv'
     response.headers['Content-type'] = 'text/csv'
     return response
-
-
-@app.route('/upload_hyper', methods=['GET', 'POST'])
-def upload_hyper():
-    if request.method == 'POST':
-        csv_file = request.files['file2']
-        if csv_file:
-            # バイト列から文字列へ変換する
-            stream = io.StringIO(csv_file.stream.read().decode("utf-8-sig"), newline="")
-            csv_reader = csv.reader(stream)
-            urls = [row[0] for row in csv_reader]
-            # リンク毎にページを読み込み、情報を取得する
-            rows = [["リンク", "販売中", "価格", "商品名", "送料込み"]]
-            for url in urls:
-                driver.get(url)
-                time.sleep(4)
-                html = driver.page_source
-                soup = BeautifulSoup(html, 'html.parser')
-                # 販売中の判定
-                if soup.select_one('body:contains("購入手続きへ")'):
-                    selling = "販売中"
-                else:
-                    selling = "×"
-                # 価格の取得
-                try:
-                    price = soup.select_one('div[data-testid="price"] span:nth-of-type(2)').text.strip()
-                except AttributeError:
-                    price = "×"
-                # 商品名の取得
-                try:
-                    name = soup.select_one('div[class^="labelContainer"] h1').text.strip()
-                except AttributeError:
-                    name = "×"
-                # 送料込みの取得
-                try:
-                    shipping = soup.select_one('mer-display-row:nth-of-type(3) span[slot="body"]').text.strip()
-                except AttributeError:
-                    shipping = "×"
-                # 1行の情報を作成
-                row = [url, selling, price, name, shipping]
-                rows.append(row)
-            
-            # 結果をHTMLに渡す
-            pairs = rows[1:]
-            session['pairs'] = pairs
-            return render_template('upload_hyper_result.html', rows=rows)
-        else:
-            return 'CSVファイルがアップロードされていません。'
-    return render_template('upload_form_hyper.html')
-
-@app.route('/csv_hyper_download', methods=['GET', 'POST'])
-def csv_hyper_download():
-    if request.method == 'POST':
-        # ペアをCSVに書き込む
-        pairs = session.get('pairs', [])
-        if not pairs:
-            return 'ペアがありません。'
-        output = io.StringIO()
-        writer = csv.writer(output)
-        writer.writerow(['リンク', '販売中', '価格', '商品名', '送料込み'])
-        writer.writerows(pairs)
-        output.seek(0)
-
-        # CSVをダウンロードする
-        response = make_response(output.getvalue())
-        response.headers['Content-Disposition'] = 'attachment; filename=pairs.csv'
-        response.mimetype = 'text/csv'
-        return response
-    else:
-        return 'Method Not Allowed'
-
-def random_choice(lst):
-    return random.choice(lst)
 
 app.jinja_env.filters['random_choice'] = random_choice
 video_files = ["video1.mp4", "video2.mp4", "video3.mp4"]
